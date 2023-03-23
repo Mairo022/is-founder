4cf9eca49e1d8bf7b6b4265b2451b7c6fb446c00
Mairo022 <118812987+Mairo022@users.noreply.github.com>
2023-03-23 23:12:21 +0200

    Add function to check whether user input matches a founder name

--- app.ts
+++ app.ts
@@ -1,15 +1,57 @@
 import { Command } from "commander";
+import * as fs from "fs";
+import * as readline from "readline";
+import { once } from 'events';
 
 function main(): void {
     userInterface()
 }
 
-function userInterface(): void {
+async function userInterface(): Promise<void> {
     const program = new Command();
     program.argument('<name>')
     program.parse(process.argv)
 
     const name: string | undefined = program.processedArgs[0]
+
+    if (name && await isFounder(name)) {
+        console.log(name, "is a founder.")
+    } else {
+        console.log(name, "is not a founder.")
+    }
+}
+
+async function isFounder(name: string): Promise<boolean> {
+    const readStream = fs.createReadStream("founders.txt")
+
+    //@ts-ignore
+    const readLine = readline.createInterface({
+        input: readStream,
+        crlfDelay: Infinity,
+        encoding: "UTF-8",
+    })
+
+    let found: boolean = false
+
+    readLine.on("line", (line) => {
+        if (line === name) {
+            found = true
+            // Stop reading and clean-up resources
+            readStream.close()
+        }
+    })
+
+    readStream.on("error", error => {
+        console.error("Stream - error reading the file:", error)
+    })
+
+    readLine.on("error", error => {
+        console.error("Readline - error reading the file:", error)
+    })
+
+    await once(readStream, 'close');
+
+    return found
 }
 
 main()
