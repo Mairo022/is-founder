26b6adcc92607faa79e2ebfe85ab8e02b797edb4
Mairo022 <118812987+Mairo022@users.noreply.github.com>
2023-03-23 22:42:42 +0200

    Add command-line interface

--- app.ts
+++ app.ts
@@ -1,5 +1,15 @@
-function main(): void {
+import { Command } from "commander";
 
+function main(): void {
+    userInterface()
+}
+
+function userInterface(): void {
+    const program = new Command();
+    program.argument('<name>')
+    program.parse(process.argv)
+
+    const name: string | undefined = program.processedArgs[0]
 }
 
 main()
